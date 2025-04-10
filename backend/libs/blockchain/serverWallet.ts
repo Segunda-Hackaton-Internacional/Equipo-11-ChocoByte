ca9efc14bcecfa7bf38936c840a1cd5db4a531d8
JuanCSUCoder <jcsu2019369@gmail.com>
2025-04-09 20:05:55 -0500

    Feat: Implementada la parte de blockchain

--- backend/libs/blockchain/serverWallet.ts
+++ backend/libs/blockchain/serverWallet.ts
@@ -1,21 +1,27 @@
 import { mplCore } from "@metaplex-foundation/mpl-core";
-import { generateSigner, keypairIdentity } from "@metaplex-foundation/umi";
+import { generateSigner, keypairIdentity, sol } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+// import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 import { readFileSync } from "fs";
 
-const umi = createUmi('https://api.devnet.solana.com')
+export const umi = createUmi('https://api.devnet.solana.com')
   .use(mplCore())
   /* .use(irysUploader({
     address: 'https://api.devnet.irys.xyz',
 })); */
 
-const signer = generateSigner(umi);
+if (process.env.USE_LOCAL_WALLET === 'true') {
+  // Use local wallet
+  const walletFile = readFileSync('./wallet.json', 'utf-8');
+  const wallet = JSON.parse(walletFile);
+  const privateKey = Uint8Array.from(wallet);
+  const serverWallet = umi.eddsa.createKeypairFromSecretKey(privateKey);
+  umi.use(keypairIdentity(serverWallet));
+} else {
+  // Use generated wallet
+  const serverWallet = generateSigner(umi);
+  umi.use(keypairIdentity(serverWallet));
+}
 
-const walletFile = readFileSync('./wallet.json', 'utf-8');
-const wallet = JSON.parse(walletFile);
-const privateKey = Uint8Array.from(wallet);
-
-const keypair = umi.eddsa.createKeypairFromSecretKey(privateKey);
-
-umi.use(keypairIdentity(keypair));
+await umi.rpc.airdrop(umi.identity.publicKey, sol(1)); // 1 SOL in lamports is 1_000_000_000
+console.log(`Airdropped 1 SOL to ${umi.identity.publicKey.toString()}`);
--- backend/routes/blockchain.ts
+++ backend/routes/blockchain.ts
@@ -1,9 +1,32 @@
 import { Router } from "express";
+import { uploadMetadata } from "../libs/blockchain/metadata";
+import { umi } from "../libs/blockchain/serverWallet";
+import { create } from "@metaplex-foundation/mpl-core";
+import { generateSigner, type PublicKey, type TransactionSignature } from "@metaplex-foundation/umi";
 
 export const blockchainRouter = Router();
 
-async function createNFT() {
-  
+interface NFT {
+  signature: TransactionSignature;
+  publicKey: PublicKey;
+}
+
+async function createNFT(name: string, attributes: any): NFT {
+  // 1. Upload metadata
+  const muri = await uploadMetadata(attributes);
+
+  // 2. Mint NFT
+  const asset = generateSigner(umi);
+  const tx = await create(umi, {
+    asset,
+    name,
+    uri: muri,
+  }).sendAndConfirm(umi);
+
+  return {
+    signature: tx.signature,
+    publicKey: asset.publicKey,
+  }
 }
 
 blockchainRouter.post('/register/batch', async (req, res) => {
