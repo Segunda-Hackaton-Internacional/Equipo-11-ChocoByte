9de85b8081c28dd71e70fa1820106821c5d232c9
JuanCSUCoder <jcsu2019369@gmail.com>
2025-04-09 19:41:05 -0500

    Feat: base de blockchain

---
+++ backend/libs/blockchain/serverWallet.ts
@@ -0,0 +1,22 @@
+import { mplCore } from "@metaplex-foundation/mpl-core";
+import { generateSigner, keypairIdentity } from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFileSync } from "fs";
+
+const umi = createUmi('https://api.devnet.solana.com')
+  .use(mplCore())
+  /* .use(irysUploader({
+    address: 'https://api.devnet.irys.xyz',
+  }) */
+);
+
+const signer = generateSigner(umi);
+
+const walletFile = readFileSync('./wallet.json', 'utf-8');
+const wallet = JSON.parse(walletFile);
+const privateKey = Uint8Array.from(wallet);
+
+const keypair = umi.eddsa.createKeypairFromSecretKey(privateKey);
+
+umi.use(keypairIdentity(keypair));
--- backend/routes/blockchain.ts
+++ backend/routes/blockchain.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
 
 export const blockchainRouter = Router();
+
+async function createNFT() {
+  
+}
+
+blockchainRouter.post('/register/batch', async (req, res) => {
+
+});
