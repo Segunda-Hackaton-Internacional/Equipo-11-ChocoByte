0b34439abc34c99df7818f7077db6285ffb18005
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2025-04-17 18:05:07 -0500

    comprar producto

--- backend/routes/shopping.ts
+++ backend/routes/shopping.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { makePurchase } from "../controllers/purchaseController";
 
 export const shoppingRouter = Router();
+
+shoppingRouter.post("/comprar", makePurchase);
