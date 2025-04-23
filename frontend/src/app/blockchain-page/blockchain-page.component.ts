cf76bae83c29f6b951b0abd63efebb6b25f0d95f
JuanCSUCoder <jcsu2019369@gmail.com>
2025-04-22 20:39:32 -0500

    Feat: Estructura de servicios

--- frontend/src/app/blockchain-page/blockchain-page.component.ts
+++ frontend/src/app/blockchain-page/blockchain-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Product } from '../../model/product';
+import { ProductsService } from '../services/products/products.service';
 
 @Component({
   selector: 'app-blockchain-page',
@@ -8,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrl: './blockchain-page.component.css'
 })
 export class BlockchainPageComponent {
+  private readonly route = inject(ActivatedRoute);
+  private productService = inject(ProductsService);
 
+  product: Product | undefined;
+
+  ngOnInit() {
+    const productId = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(Number(productId)).subscribe(product => {
+      this.product = product;
+    }
+    );
+  }
 }
