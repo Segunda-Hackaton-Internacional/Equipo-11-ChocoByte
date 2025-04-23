2775d64f285dfc625fe97c0aa7ccf9acc1028672
JuanCSUCoder <jcsu2019369@gmail.com>
2025-04-22 21:04:11 -0500

    Feat: Completed blockchain backend and model interfaces

--- frontend/src/app/services/blockchain/blockchain.service.ts
+++ frontend/src/app/services/blockchain/blockchain.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NFTRegistry } from '../../../model/nft-registry';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
+  private readonly httpClient = inject(HttpClient);
 
   constructor() { }
+
+  getProductNFTMetadata(productId: number): Observable<NFTRegistry> {
+    
+  }
 }
--- frontend/src/model/nft-registry.ts
+++ frontend/src/model/nft-registry.ts
@@ -1,4 +1,8 @@
+import { NFTMetadata } from "./nft-metadata";
+
 export interface NFTRegistry {
   name?: string;
   metdata_uri?: string;
+  attributes?: NFTMetadata;
+  publicKey?: string;
 }
