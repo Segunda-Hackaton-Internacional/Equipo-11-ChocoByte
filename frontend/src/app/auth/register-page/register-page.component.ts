8473f7ab9170fcfc006d31fc17d8d74daa76018e
JuanCSUCoder <jcsu2019369@gmail.com>
2025-05-18 15:07:45 -0500

    Fix: Input touch detection

--- frontend/src/app/auth/register-page/register-page.component.ts
+++ frontend/src/app/auth/register-page/register-page.component.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SmartInputComponent } from "../../components/smart-input/smart-input.component";
+import { passwordConfirmationValidator } from '../../../lib/form/validators/passConfirm';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, ReactiveFormsModule, SmartInputComponent],
+  imports: [NavbarComponent, FooterComponent, ReactiveFormsModule, SmartInputComponent, CommonModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
@@ -20,6 +22,10 @@ export class RegisterPageComponent {
     confirmPassword: ['', [Validators.required]],
     firstName: ['', [Validators.required]],
     lastName: [''],
+  }, {
+    validators: [
+      passwordConfirmationValidator('password', 'confirmPassword')
+    ]
   });
 
   public onSubmit() {
