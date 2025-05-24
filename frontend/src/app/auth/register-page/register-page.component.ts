4fd5469ca7fac35983f075436286a5bc90716385
JuanCSUCoder <jcsu2019369@gmail.com>
2025-05-24 14:00:58 -0500

    Feat: Add profile

--- frontend/src/app/auth/register-page/register-page.component.ts
+++ frontend/src/app/auth/register-page/register-page.component.ts
@@ -6,6 +6,7 @@ import { SmartInputComponent } from "../../components/smart-input/smart-input.co
 import { passwordConfirmationValidator } from '../../../lib/form/validators/passConfirm';
 import { CommonModule } from '@angular/common';
 import { UsersAdapter } from '../../../lib/auth/usersAdapter';
+import { updateProfile } from 'firebase/auth';
 
 @Component({
   selector: 'app-register-page',
@@ -39,9 +40,12 @@ export class RegisterPageComponent {
       this.registerForm.value.email as string,
       this.registerForm.value.password as string
     ).then((result) => {
-      if (result.result) {
+      if (result && result.result && result.user) {
         this.registrado = true;
         this.error = false;
+        updateProfile(result.user, {
+          displayName: `${this.registerForm.value.firstName} ${this.registerForm.value.lastName}`,
+        })
       } else {
         this.registrado = false;
         this.error = true;
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from '../../../lib/auth/firebase';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +10,32 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  public loggedIn: boolean = false;
+  public user: any;
+
+  ngOnInit() {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if (user) {
+        this.loggedIn = true;
+        this.user = {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL
+        };
+      } else {
+        this.loggedIn = false;
+      }
+    });
+  }
+
+  logout() {
+    firebaseAuth.signOut().then(() => {
+      this.loggedIn = false;
+      this.user = null;
+    }).catch((error) => {
+      console.error('Error signing out: ', error);
+    });
+  }
+}
